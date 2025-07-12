ba4cd9792214f1dc4ae0aac286003c9be1754435
flamrdevs <flamrdevs@gmail.com>
2025-07-13 01:51:35 +0700

    test: improve

--- packages/xignal/test/standard-schema.node.test.ts
+++ packages/xignal/test/standard-schema.node.test.ts
@@ -9,31 +9,10 @@ import { z } from "zod";
 vt.describe("standard-schema", () => {
 	vt.describe("arktype", () => {
 		vt.it("should work", () => {
-			const state = xignalStandardSchema.state(
-				type({
-					name: "string",
-				}),
-				{
-					name: "1",
-				},
-			);
-			const computed = xignalStandardSchema.computed(
-				type({
-					length: "number",
-				}),
-				() => ({
-					length: state.get().name.length,
-				}),
-			);
-
-			vt.expect(() => {
-				xignalStandardSchema.state(
-					type({
-						key: "string",
-					}),
-					[] as unknown as { key: string },
-				);
-			}).toThrow();
+			const state = xignalStandardSchema.state(type({ name: "string" }), { name: "1" });
+			const computed = xignalStandardSchema.computed(type({ length: "number" }), () => ({
+				length: state.get().name.length,
+			}));
 
 			vt.expect(state.get()).toEqual({ name: "1" });
 			vt.expect(computed.get()).toEqual({ length: 1 });
@@ -42,40 +21,32 @@ vt.describe("standard-schema", () => {
 
 			vt.expect(state.get()).toEqual({ name: "12" });
 			vt.expect(computed.get()).toEqual({ length: 2 });
+		});
+
+		vt.it("should error", () => {
+			const state = xignalStandardSchema.state(type({ name: "string" }), { name: "1" });
+			const computed = xignalStandardSchema.computed(type({ is: "boolean" }), () => [] as unknown as { is: boolean });
+
+			vt.expect(() => {
+				xignalStandardSchema.state(type({ key: "string" }), [] as unknown as { key: string });
+			}).toThrow();
 
 			vt.expect(() => {
 				state.set([] as unknown as { name: string });
+			}).toThrow();
+
+			vt.expect(() => {
+				computed.get();
 			}).toThrow();
 		});
 	});
 
 	vt.describe("valibot", () => {
 		vt.it("should work", () => {
-			const state = xignalStandardSchema.state(
-				v.object({
-					name: v.string(),
-				}),
-				{
-					name: "1",
-				},
-			);
-			const computed = xignalStandardSchema.computed(
-				v.object({
-					length: v.number(),
-				}),
-				() => ({
-					length: state.get().name.length,
-				}),
-			);
-
-			vt.expect(() => {
-				xignalStandardSchema.state(
-					v.object({
-						key: v.string(),
-					}),
-					[] as unknown as { key: string },
-				);
-			}).toThrow();
+			const state = xignalStandardSchema.state(v.object({ name: v.string() }), { name: "1" });
+			const computed = xignalStandardSchema.computed(v.object({ length: v.number() }), () => ({
+				length: state.get().name.length,
+			}));
 
 			vt.expect(state.get()).toEqual({ name: "1" });
 			vt.expect(computed.get()).toEqual({ length: 1 });
@@ -89,35 +60,34 @@ vt.describe("standard-schema", () => {
 				state.set([] as unknown as { name: string });
 			}).toThrow();
 		});
+
+		vt.it("should error", () => {
+			const state = xignalStandardSchema.state(v.object({ name: v.string() }), { name: "1" });
+			const computed = xignalStandardSchema.computed(
+				v.object({ is: v.boolean() }),
+				() => [] as unknown as { is: boolean },
+			);
+
+			vt.expect(() => {
+				xignalStandardSchema.state(v.object({ key: v.string() }), [] as unknown as { key: string });
+			}).toThrow();
+
+			vt.expect(() => {
+				state.set([] as unknown as { name: string });
+			}).toThrow();
+
+			vt.expect(() => {
+				computed.get();
+			}).toThrow();
+		});
 	});
 
 	vt.describe("zod", () => {
 		vt.it("should work", () => {
-			const state = xignalStandardSchema.state(
-				z.object({
-					name: z.string(),
-				}),
-				{
-					name: "1",
-				},
-			);
-			const computed = xignalStandardSchema.computed(
-				z.object({
-					length: z.number(),
-				}),
-				() => ({
-					length: state.get().name.length,
-				}),
-			);
-
-			vt.expect(() => {
-				xignalStandardSchema.state(
-					z.object({
-						key: z.string(),
-					}),
-					[] as unknown as { key: string },
-				);
-			}).toThrow();
+			const state = xignalStandardSchema.state(z.object({ name: z.string() }), { name: "1" });
+			const computed = xignalStandardSchema.computed(z.object({ length: z.number() }), () => ({
+				length: state.get().name.length,
+			}));
 
 			vt.expect(state.get()).toEqual({ name: "1" });
 			vt.expect(computed.get()).toEqual({ length: 1 });
@@ -126,9 +96,25 @@ vt.describe("standard-schema", () => {
 
 			vt.expect(state.get()).toEqual({ name: "12" });
 			vt.expect(computed.get()).toEqual({ length: 2 });
+		});
+
+		vt.it("should error", () => {
+			const state = xignalStandardSchema.state(z.object({ name: z.string() }), { name: "1" });
+			const computed = xignalStandardSchema.computed(
+				z.object({ is: z.boolean() }),
+				() => [] as unknown as { is: boolean },
+			);
+
+			vt.expect(() => {
+				xignalStandardSchema.state(z.object({ key: z.string() }), [] as unknown as { key: string });
+			}).toThrow();
 
 			vt.expect(() => {
 				state.set([] as unknown as { name: string });
+			}).toThrow();
+
+			vt.expect(() => {
+				computed.get();
 			}).toThrow();
 		});
 	});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vitest/config";
 
+const packages = "*";
+
 export default defineConfig({
 	test: {
 		projects: [
-			"packages/*/vitest.config.ts",
+			`packages/${packages}/vitest.config.ts`,
 			{
 				test: {
-					include: ["packages/*/test/**/*.node.test.ts"],
+					include: [`packages/${packages}/test/**/*.node.test.ts`],
 					name: "node",
 					environment: "node",
 				},
